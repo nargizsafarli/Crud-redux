4712c2be7cd6e97c3095e393ac0208c304e02427
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-04-17 23:12:41 +0400

    solved deploy problem

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import store from './Store/Store.js'
+import store from "../Store/store.js"
+
+
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
